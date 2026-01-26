be710267b7346c094049e587092299983585e391
DaleDiamond <dale.diamond@aol.com>
2026-01-25 23:03:37 -0700

    adding command

---
+++ Probe.py
@@ -0,0 +1,15 @@
+#Topic 3_Networking and Network Reconnaissance
+#Diamond Dale
+#Grand Canyon University
+#ITT-111
+#Professor Geondrell Jackson
+#1/25/26
+
+import subprocess
+
+def ping_host(network):
+    for host in range(1,256):
+        ip = network +str(host)
+        command = ['ping', '-n', '1', ip]
+        result = subprocess.call(command, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+
